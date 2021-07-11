7ca3c7f7bddd081ec2221cb014ae0849d56f61fc
Chris <cmsl4y3r@gmail.com>
2021-07-11 17:50:26 -0600

    initial commit

---
+++ product.py
@@ -0,0 +1,21 @@
+class Product:
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
+        
+    def update_price(self, percent_change, is_increased):
+        self.percent_change = percent_change
+        self.is_increased = is_increased
+    
+        
+        if(is_increased==True):
+            self.price += (self.price*percent_change) 
+    
+    
+    
+    def print_info(self):
+        print(f"Item : {self.name} \nCategory : {self.category} \nCost : ${self.price}")
+
+new_product = Product("TV", 3000, "electronics")
+new_product.print_info()
---
+++ store.py
@@ -0,0 +1,29 @@
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.product_list = []
+    
+    def add_product(self, new_product):
+        self.product_list.append(new_product)
+        
+    def sell_product(self, id):
+        print(f"Removing {self.product_list[id]}")
+        self.product_list.remove(self.product_list[id])
+        
+    def inflation(self, percent_increase):
+        self.percent_increase = percent_increase * .001
+        
+    
+    def set_clearance(self, category, percent_discount):
+        
+
+
+new_store = Store("Albertsons")
+new_store.add_product("TV")
+new_store.add_product("Blender")
+print(new_store.product_list)
+new_store.sell_product(1)
+print(new_store.product_list)
+for i in new_store.product_list:
+    print(id(i))
+  
